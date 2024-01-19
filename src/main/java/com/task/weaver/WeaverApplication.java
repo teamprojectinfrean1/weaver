f0ff80683d383a9d6078f7f71c2b7b251f0b301a
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-01-19 22:48:32 +0900

    first commit

---
+++ src/main/java/com/task/weaver/WeaverApplication.java
@@ -0,0 +1,13 @@
+package com.task.weaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/weaver/WeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.weaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
