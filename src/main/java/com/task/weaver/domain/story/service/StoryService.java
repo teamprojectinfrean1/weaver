0a518060e0491ba2f5f922c962ebc5bd2eb90b1d
조영준 <joyoungjun8590@hotmail.com>
2024-01-21 22:48:44 +0900

    동적 쿼리를 위한 Condition 추가, StoryService V1 인터페이스 정의 끝

--- src/main/java/com/task/weaver/domain/story/service/StoryService.java
+++ src/main/java/com/task/weaver/domain/story/service/StoryService.java
@@ -3,11 +3,13 @@ package com.task.weaver.domain.story.service;
 import com.task.weaver.common.exception.AuthorizationException;
 import com.task.weaver.common.exception.NotFoundException;
 import com.task.weaver.domain.issue.Issue;
+import com.task.weaver.domain.issue.IssueMention;
 import com.task.weaver.domain.project.Project;
 import com.task.weaver.domain.story.dto.request.RequestCreateStory;
 import com.task.weaver.domain.story.entity.Story;
 import com.task.weaver.domain.user.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StoryService {
 
@@ -16,18 +18,24 @@ public interface StoryService {
     Story getStory (Issue issue)
             throws NotFoundException, AuthorizationException;
 
-    Page<Story> getStories (Long projectId)
+    Page<Story> getStories (Long projectId, Pageable pageable)
             throws NotFoundException, AuthorizationException;
-    Page<Story> getStories (Project project)
+    Page<Story> getStories (Project project, Pageable pageable)
             throws NotFoundException, AuthorizationException;
-    Page<Story> getStories (Project project, User user)
+    Page<Story> getStories (Project project, User user, Pageable pageable)
             throws NotFoundException, AuthorizationException;
-    Page<Story> getStories (Project project, Long userId)
+    Page<Story> getStories (Project project, Long userId, Pageable pageable)
             throws NotFoundException, AuthorizationException;
-    Page<Story> getStories (Long projectId, Long User)
+    Page<Story> getStories (Long projectId, Long User, Pageable pageable)
             throws NotFoundException, AuthorizationException;
 
-    Story add (Story story);
-    Story add (RequestCreateStory request);
+    Story addStory (Story story);
+    Story addStory (Story story, IssueMention issueMention);
+    Story addStory (RequestCreateStory request);
 
+    void deleteStory (Story story);
+    void deleteStory (Long storyId);
+
+    Story updateStory (Story originalStory, Story newStory);
+    Story updateStory (Long originalStoryId, Story newStory);
 }
