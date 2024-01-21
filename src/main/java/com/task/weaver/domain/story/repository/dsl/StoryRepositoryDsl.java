66c47fc06bc7f8fbe433bd725945878f973788b6
조영준 <joyoungjun8590@hotmail.com>
2024-01-21 22:52:48 +0900

    [StoryService] QueryDSL 베이스 작업

---
+++ src/main/java/com/task/weaver/domain/story/repository/dsl/StoryRepositoryDsl.java
@@ -0,0 +1,5 @@
+package com.task.weaver.domain.story.repository.dsl;
+
+public interface StoryRepositoryDsl {
+
+}
---
+++ src/main/java/com/task/weaver/domain/story/repository/dsl/impl/StoryRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.task.weaver.domain.story.repository.dsl.impl;
+
+import com.task.weaver.domain.story.repository.dsl.StoryRepositoryDsl;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StoryRepositoryImpl implements StoryRepositoryDsl {
+
+}
