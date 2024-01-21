bd9d847cd81228998198e05531c0f76127230fff
wkadh <wkadht0619@gmail.com>
2024-01-21 12:18:52 +0900

    feat(Issue) : Issue 관련 기능 추가

---
+++ src/main/java/com/task/weaver/domain/issue/Issue.java
@@ -0,0 +1,48 @@
+package com.task.weaver.domain.issue;
+
+import com.task.weaver.domain.status.StatusTag;
+import com.task.weaver.domain.task.Task;
+import com.task.weaver.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Issue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "issue_id")
+    private Long issueId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "status_tag_id")
+    private StatusTag statusTag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "create_user_id")
+    private User user;
+
+    @Column(name = "issue_name", length = 100)
+    private String issueName;
+
+    @Column(name = "issue_type", length = 100)
+    private String issueType;
+
+    @Column(name = "issue_text")
+    private String issueText;
+
+    private LocalDateTime created;
+}
---
+++ src/main/java/com/task/weaver/domain/issue/IssueMention.java
@@ -0,0 +1,32 @@
+package com.task.weaver.domain.issue;
+
+import com.task.weaver.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "ISSUE_MENTION")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class IssueMention {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "issue_mention_id")
+    private Long issueMentionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_id")
+    private Issue issue;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentioned_user_id")
+    private User user;
+
+    @Column(name ="is_check", columnDefinition = "TINYINT(1)")
+    private boolean isCheck;
+}
