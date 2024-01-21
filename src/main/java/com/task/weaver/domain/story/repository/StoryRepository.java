052cb472e3138b31a54c6df55c5cf70c3ce6e1d9
조영준 <joyoungjun8590@hotmail.com>
2024-01-21 22:53:07 +0900

    [StoryService] QueryDSL 베이스 작업

--- src/main/java/com/task/weaver/domain/story/repository/StoryRepository.java
+++ src/main/java/com/task/weaver/domain/story/repository/StoryRepository.java
@@ -1,7 +1,9 @@
 package com.task.weaver.domain.story.repository;
 
 import com.task.weaver.domain.story.entity.Story;
+import com.task.weaver.domain.story.repository.dsl.StoryRepositoryDsl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoryRepository extends JpaRepository<Story, Long> {
+public interface StoryRepository extends JpaRepository<Story, Long>, StoryRepositoryDsl {
+
 }
