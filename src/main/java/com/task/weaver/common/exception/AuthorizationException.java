df8c7b88ea09986d3e5a9185e0101a2d6143dc1d
조영준 <joyoungjun8590@hotmail.com>
2024-01-21 22:35:12 +0900

    Story 패키지 구조 결정, 최상단에 Spring 의존 코드(filter, interceptor, bean) 등을 담을 수 있는 패키지 생성

---
+++ src/main/java/com/task/weaver/common/exception/AuthorizationException.java
@@ -0,0 +1,4 @@
+package com.task.weaver.common.exception;
+
+public class AuthorizationException extends RuntimeException {
+}
--- src/main/java/com/task/weaver/domain/story/service/StoryService.java
+++ src/main/java/com/task/weaver/domain/story/service/StoryService.java
@@ -1,17 +1,33 @@
 package com.task.weaver.domain.story.service;
 
+import com.task.weaver.common.exception.AuthorizationException;
+import com.task.weaver.common.exception.NotFoundException;
 import com.task.weaver.domain.issue.Issue;
+import com.task.weaver.domain.project.Project;
 import com.task.weaver.domain.story.entity.Story;
 import com.task.weaver.domain.user.User;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface StoryService {
 
 
-    Story getStory (Long storyId);
-    List<Story> getStory (User user);
-    Story getStory (Issue issue);
+    Story getStory (Long storyId)
+            throws NotFoundException, AuthorizationException;
+    Story getStory (Issue issue)
+            throws NotFoundException, AuthorizationException;
+
+    Page<Story> getStories (Long projectId)
+            throws NotFoundException, AuthorizationException;
+    Page<Story> getStories (Project project)
+            throws NotFoundException, AuthorizationException;
+    Page<Story> getStories (Project project, User user)
+            throws NotFoundException, AuthorizationException;
+    Page<Story> getStories (Project project, Long userId)
+            throws NotFoundException, AuthorizationException;
+    Page<Story> getStories (Long projectId, Long User)
+            throws NotFoundException, AuthorizationException;
 
 
 }
