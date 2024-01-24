a0fbe86c5d5b5d08ccfe3b9d400a5f5325e1b013
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-01-24 16:04:06 +0900

    gobal(response) : 기본 응답 형태 추가

--- src/main/java/com/task/weaver/common/response/DefaultResponse.java
+++ src/main/java/com/task/weaver/common/response/DefaultResponse.java
@@ -1,2 +1,16 @@
-package com.task.weaver.common.response;public class DefaultResponse {
+package com.task.weaver.common.response;
+
+import lombok.Getter;
+
+@Getter
+public abstract class DefaultResponse {
+
+    protected int status;
+
+    protected String message;
+
+    protected DefaultResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
