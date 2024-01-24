fd8a2a52aafa34c4c49cb882a574719b818e4746
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-01-24 16:05:28 +0900

    gobal(exception) : 기본 예외 구조 정의

--- src/main/java/com/task/weaver/common/exception/BusinessException.java
+++ src/main/java/com/task/weaver/common/exception/BusinessException.java
@@ -1,2 +1,22 @@
-package com.task.weaver.common.exception;public class BusinessException {
+package com.task.weaver.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BusinessException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+    protected BusinessException(ErrorCode errorCode, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(errorCode.getMessage(), cause, enableSuppression, writableStackTrace);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode, Throwable cause) {
+        this(errorCode, cause, true, true);
+    }
+
+    public ErrorCode getErrorCode(){
+        return errorCode;
+    }
 }
--- src/main/java/com/task/weaver/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/task/weaver/common/exception/GlobalExceptionHandler.java
@@ -1,2 +1,53 @@
-package com.task.weaver.common.exception;public class GlobalExceptionHandler {
+package com.task.weaver.common.exception;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * controller 까지 올라온 예외를 처리하는 클래스
+ */
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex
+    ) {
+        log.error("MethodArgumentNotValidException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, ex.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageConversionException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMessageConversionException(
+            HttpMessageConversionException ex
+    ) {
+        log.error("HttpMessageConversionException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_TYPE_VALUE);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException ex) {
+        log.error("BindException", ex);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, ex.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.warn("BusinessException", e);
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response = ErrorResponse.of(errorCode);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
 }
