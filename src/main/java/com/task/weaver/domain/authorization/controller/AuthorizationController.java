dfb05305219a35449feeeca6bbd0384a7cf83161
yerim8373 <yerim8373@naver.com>
2024-02-16 16:52:05 +0900

    feat : Authotization - login

--- src/main/java/com/task/weaver/domain/authorization/controller/AuthorizationController.java
+++ src/main/java/com/task/weaver/domain/authorization/controller/AuthorizationController.java
@@ -1,8 +1,10 @@
 package com.task.weaver.domain.authorization.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +22,14 @@ public class AuthorizationController {
 	private final AuthorizationService authorizationService;
 
 	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody RequestSignIn requestSignIn, HttpServletResponse httpServletResponse) {
+	public ResponseEntity<?> login(@RequestBody RequestSignIn requestSignIn) {
+		// access token -> header? body? cookie?
 
 		TokenResponse tokenResponse = authorizationService.login(requestSignIn);
-		return ResponseEntity.ok().body(tokenResponse);
+		// access token만 return
+		return ResponseEntity.ok().body(tokenResponse.accessToken());
 	}
+
+	// @GetMapping("/reissue")
+	// public ResponseEntity<?> reissue(@Request)
 }
