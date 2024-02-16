d4b4c56fd2aa66d098ef8690440fa88109216509
yerim8373 <yerim8373@naver.com>
2024-02-16 22:39:39 +0900

    feat : authorization - login 수정

--- src/main/java/com/task/weaver/domain/authorization/controller/AuthorizationController.java
+++ src/main/java/com/task/weaver/domain/authorization/controller/AuthorizationController.java
@@ -1,5 +1,7 @@
 package com.task.weaver.domain.authorization.controller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,19 +16,34 @@ import com.task.weaver.domain.authorization.service.AuthorizationService;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthorizationController {
 	private final AuthorizationService authorizationService;
 
 	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody RequestSignIn requestSignIn) {
+	public ResponseEntity<?> login(@RequestBody RequestSignIn requestSignIn, HttpServletResponse res) {
 		// access token -> header? body? cookie?
-
 		TokenResponse tokenResponse = authorizationService.login(requestSignIn);
-		// access token만 return
+
+		// refresh token cookie에 담기
+		ResponseCookie cookie = ResponseCookie.from("refresh-token", tokenResponse.refreshToken())
+			.maxAge(60 * 60 * 24 * 15)
+			.httpOnly(true)
+			.secure(true)
+			.domain("")
+			.path("/")
+			.sameSite("None")
+			.build();
+
+		res.setHeader("Set-Cookie", cookie.toString());
+		log.info(tokenResponse.accessToken());
+
+		// access token body에 담아 return
 		return ResponseEntity.ok().body(tokenResponse.accessToken());
 	}
 
