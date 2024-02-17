ca2848acb772dfb5659343cf1e20cff4d4d30fc1
Ewan Jee <830jiforcareer@gmail.com>
2024-02-17 01:18:30 -0500

    feat Chatting User 연관관계 변경

--- src/main/java/com/task/weaver/domain/websocket/entity/Chatting.java
+++ src/main/java/com/task/weaver/domain/websocket/entity/Chatting.java
@@ -16,7 +16,7 @@ public class Chatting {
     @Id @GeneratedValue
     private Long id;
     private String content;
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
     @ManyToOne(fetch = FetchType.LAZY)
