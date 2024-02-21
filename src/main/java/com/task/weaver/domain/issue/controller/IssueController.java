97c93e39c1051ca400743afa4f45f6edb9cc3fd7
yerim8373 <yerim8373@naver.com>
2024-02-21 19:31:11 +0900

    feat : Issue - 단건 조회, 삭제 swagger 작성

--- src/main/java/com/task/weaver/domain/issue/controller/IssueController.java
+++ src/main/java/com/task/weaver/domain/issue/controller/IssueController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.task.weaver.domain.issue.dto.response.IssueResponse;
 import com.task.weaver.domain.issue.service.IssueService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Issue Controller", description = "이슈 관련 컨트롤러")
 @RestController
 @RequestMapping("/api/v1/issue")
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ public class IssueController {
 	private final IssueService issueService;
 	// private final DataResponse dataResponse;
 
+	@Operation(summary = "이슈 단건 조회", description = "issueId로 이슈 단건 조회")
 	@GetMapping("/{id}")
 	public ResponseEntity<?> getIssueInfo(@PathVariable Long issueId){
 		IssueResponse issueResponse = issueService.getIssue(issueId);
@@ -27,6 +31,7 @@ public class IssueController {
 			.body(issueResponse);
 	}
 
+	@Operation(summary = "이슈 삭제", description = "issueId로 이슈 삭제")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> removeIssue(@PathVariable Long issueId){
 		issueService.deleteIssue(issueId);
