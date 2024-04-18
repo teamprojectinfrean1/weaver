888cb300b35fdaf3190b78c957100e6b56b93483
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-04-18 17:21:19 +0900

    refactor(chatting) : user -> member 변경

--- src/main/java/com/task/weaver/domain/websocket/entity/Chatting.java
+++ src/main/java/com/task/weaver/domain/websocket/entity/Chatting.java
@@ -1,6 +1,7 @@
 package com.task.weaver.domain.websocket.entity;
 
 import com.task.weaver.domain.BaseEntity;
+import com.task.weaver.domain.authorization.entity.Member;
 import com.task.weaver.domain.member.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,7 +26,7 @@ public class Chatting extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    private User user;
+    private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chatting_room_id")
