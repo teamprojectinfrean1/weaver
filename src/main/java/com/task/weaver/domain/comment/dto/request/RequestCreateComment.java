6a7a780d4a3aa524abbc2e882bdd5a66c8f162a0
Ewan Jee <830jiforcareer@gmail.com>
2024-04-30 11:45:46 -0400

    edit : commentBody로 필드명 변경

--- src/main/java/com/task/weaver/domain/comment/dto/request/RequestCreateComment.java
+++ src/main/java/com/task/weaver/domain/comment/dto/request/RequestCreateComment.java
@@ -10,6 +10,6 @@ public record RequestCreateComment
         (
                 UUID writerId,
                 UUID issueId,
-                String body
+                String commentBody
         ) {
 }
