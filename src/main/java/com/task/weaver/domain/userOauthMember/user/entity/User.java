b45b3e1bcf648851df5cad635668108d3de225bd
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-05-03 22:12:12 +0900

    rds(table) : table name to uppercase

--- src/main/java/com/task/weaver/domain/userOauthMember/user/entity/User.java
+++ src/main/java/com/task/weaver/domain/userOauthMember/user/entity/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.net.URL;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ import org.hibernate.annotations.GenericGenerator;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Table(name = "USER")
 @Setter
 @Builder
 public class User extends BaseEntity implements UserOauthMember {
