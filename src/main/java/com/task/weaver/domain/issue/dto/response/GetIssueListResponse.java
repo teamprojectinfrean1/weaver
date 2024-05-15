1ce2f0f849c365157262775142f62994f976b0ef
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-05-15 18:52:33 +0900

    refactor(issue) : 조회 리스폰스 null 허용

--- src/main/java/com/task/weaver/domain/issue/dto/response/GetIssueListResponse.java
+++ src/main/java/com/task/weaver/domain/issue/dto/response/GetIssueListResponse.java
@@ -1,6 +1,8 @@
 package com.task.weaver.domain.issue.dto.response;
 
 import com.task.weaver.domain.issue.entity.Issue;
+import com.task.weaver.domain.member.entity.Member;
+import java.util.Optional;
 import lombok.Builder;
 
 import java.net.URL;
@@ -23,9 +25,9 @@ public record GetIssueListResponse(UUID issueId,
 				.issueStatus(String.valueOf(issue.getStatus()))
 				.taskId(issue.getTask().getTaskId())
 				.taskTitle(issue.getTask().getTaskTitle())
-				.assigneeId(issue.getAssignee().getId())
-				.assigneeNickname(issue.getAssignee().resolveMemberByLoginType().getNickname())
-				.assigneeProfileImage(issue.getAssignee().resolveMemberByLoginType().getProfileImage())
+				.assigneeId(Optional.ofNullable(issue.getAssignee()).map(Member::getId).orElse(null))
+				.assigneeNickname(Optional.ofNullable(issue.getAssignee()).map(member -> member.resolveMemberByLoginType().getNickname()).orElse(null))
+				.assigneeProfileImage(Optional.ofNullable(issue.getAssignee()).map(member -> member.resolveMemberByLoginType().getProfileImage()).orElse(null))
 				.build();
 	}
 }
