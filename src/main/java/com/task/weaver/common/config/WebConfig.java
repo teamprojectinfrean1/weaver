7ba3bd0cee9dc0e3697183a3892d7bf32aa6e053
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-07-10 01:12:55 +0900

    refactor(cookie) : URL 명시

--- src/main/java/com/task/weaver/common/config/WebConfig.java
+++ src/main/java/com/task/weaver/common/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://https://taskstagram.info")
+                .allowedOrigins("https://taskstagram.info")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .exposedHeaders("*");
